9bbe5db63562310b82a7bb5e793c98c3d39781a2
Othon Gomes <othonmarcello@hotmail.com>
2023-08-07 16:55:09 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
